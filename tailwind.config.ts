051546fab0ac60b877ef6a1174cf2178275cdea9
yunitarchive <fenyyunita.line@gmail.com>
2024-10-11 08:55:21 +0700

    initialize transform to nextjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,37 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "black-400":"#0B0C0E",
+        "black-600":"#0B0C0E",
+        "neutral-400":"#F4F7FA",
+        "gray-400": "#C7D0D9",
+      
+
+      },
+      fontFamily:{
+        dmsans:["DM Sans", "sans-serif"],
+        
+      },
+      content: {
+        profilephoto: "url('./assets/profile_photo.png')",
+       
       },
     },
+    screens:{
+      xs:"320px",
+      sm:"480px",
+      md:"768px",
+      lg:"1025px",
+    },
+    animation: {
+      'infinite-scroll': 'infinite-scroll 15s linear infinite',
+    },
+    keyframes: {
+      'infinite-scroll': {
+        from: { transform: 'translateX(0)' },
+        to: { transform: 'translateX(-100%)' },
+      }
+    }
   },
   plugins: [],
 };
